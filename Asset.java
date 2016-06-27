4f68c813b1be2a12a08aec3785c5a1c3c9809f64
Vedavi Balaji <vedavi_balaji@trimble.com>
2016-06-27 16:06:41 +0530

    Added Asset.java

---
+++ Asset.java
@@ -0,0 +1,17 @@
+package SourceCode;
+
+/**
+ * Created by vbalaji1 on 27/06/16.
+ */
+
+public class Asset {
+    String Name;
+    String UniqueID;
+
+   Asset(String name,String uniqueID)
+   {
+       this.Name = name;
+       this.UniqueID = uniqueID;
+   }
+   
+}
